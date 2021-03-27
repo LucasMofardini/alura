2d5c28b780fb68828fba790a536486797deb5efa
Lucas Mofardini <lucasmofardini@gmail.com>
2021-03-26 21:43:14 -0300

    [PHP] Object conta

--- Php/orientaçaoAObjetos/src/conta.php
+++ Php/orientaçaoAObjetos/src/conta.php
@@ -1,10 +1,13 @@
-<?php
-
-function criarConta(string $cpf, string $nomeTitular, float $saldo): array {
-  return [
-    $cpf=>[
-      'titular' => $nomeTitular,
-      'saldo' => $saldo,
-    ]
-  ];
+<?php 
+//Tipo conta 
+class Conta{
+  //Definição dos dados da conta
+  public string $cpfTitular;
+  public string  $nometitular;
+  public float $saldo;
 }
+$contaNova = new Conta();
+$contaNova->cpcTitular = '389.886.878-84';
+$contaNova->nomeTitular = 'Lucas';
+$contaNova->saldo = 300;
+echo $contaNova->saldo;
