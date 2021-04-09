7caaa17a5fcd28af2034ea193997d126cdb72393
Lucas Mofardini <lucasmofardini@gmail.com>
2021-04-08 23:33:05 -0300

    [PHP WEB] PHP 1

---
+++ Php/php-web/phpweb-master/index.php
@@ -0,0 +1,31 @@
+<?php
+require_once 'Artigo.php';
+$artigo = new Artigo();
+$artigos = $artigo->exibirTodos();
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <title>Meu Blog - Lucas</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+    <div id="container">
+        <h1>Meu Blog</h1>
+        <?php foreach($artigos as $artigo) { ?>
+        <h2>
+            <a href="<?php echo $artigo['link']; ?>">
+                <?php echo $artigo['titulo']; ?>
+            </a>
+        </h2>
+        <p>
+        <?php echo $artigo['conteudo']; ?>
+        </p>
+      <?php } ?>
+    </div>
+</body>
+
+</html>
