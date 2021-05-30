fe6806510a97c74953310c67c9b147c47c89ab08
Lucas Mofardini <lucasmofardini@gmail.com>
2021-05-29 23:36:17 -0300

    [PHP] EXCEPTION PHP 2

--- Php/exception/projeto/pilha.php
+++ Php/exception/projeto/pilha.php
@@ -3,13 +3,19 @@
 function funcao1()
 {
     echo 'Entrei na função 1' . PHP_EOL;
-    funcao2();
+    try {funcao2();}
+    catch(RuntimeException | DivisionByZeroError  $problema){
+        echo 'Exception de : ' . $problema->getMessage() . ' Na Linha : '. $problema->getLine() . ' No CallStack : ' . PHP_EOL . $problema->getTraceAsString() . PHP_EOL;
+    }
     echo 'Saindo da função 1' . PHP_EOL;
 }
 
 function funcao2()
 {
     echo 'Entrei na função 2' . PHP_EOL;
+    $divisao = intdiv(4,0);
+    $arrayFixo = new SplFixedArray(2);
+    $arrayFixo[3] = 'Lucas';
     for ($i = 1; $i <= 5; $i++) {
         echo $i . PHP_EOL;
     }
