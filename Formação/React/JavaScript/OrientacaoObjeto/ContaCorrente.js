f4fac773ae203f3d8915c2482f27c8b514261372
Lucas Mofardini <lucasmofardini@gmail.com>
2021-08-09 22:51:35 -0300

    [JS] Orientação A Objeto - 5

--- Formação/React/JavaScript/OrientacaoObjeto/ContaCorrente.js
+++ Formação/React/JavaScript/OrientacaoObjeto/ContaCorrente.js
@@ -1,9 +1,10 @@
 import { Cliente } from './Cliente.js';
 export class ContaCorrente{
- 
+    static numeroDeContas = 0;
     agencia;
     _cliente;
-    
+    //Proposta de implementação de campos privados em JS https://github.com/tc39/proposal-class-fields#private-fields
+    _saldo = 0;
     set cliente(novoCliente){
         if(novoCliente instanceof Cliente){
             this._cliente = novoCliente;
@@ -12,9 +13,14 @@ export class ContaCorrente{
     get cliente(){
         return this._cliente;
     }
-    //Proposta de implementação de campos privados em JS https://github.com/tc39/proposal-class-fields#private-fields
-    _saldo = 0;
-
+    get saldo(){
+        return this._saldo;
+    }
+    constructor(cliente, agencia){
+        this.agencia = agencia;
+        this.cliente = cliente;
+        ContaCorrente.numeroDeContas++;
+    }
     sacar(valorSaque){
            
         if(valorSaque > this._saldo){
@@ -32,9 +38,6 @@ export class ContaCorrente{
         return valorDeposito;
     }
     mostraSaldo(){
-        return this._saldo;
-    }
-    get saldo(){
         return this._saldo;
     }
     transferir(valor, conta){
--- Formação/React/JavaScript/OrientacaoObjeto/index.js
+++ Formação/React/JavaScript/OrientacaoObjeto/index.js
@@ -1,35 +1,21 @@
 import { Cliente } from "./Cliente.js";
 import { ContaCorrente } from "./ContaCorrente.js";
 
-const cliente1 = new Cliente();
-cliente1.nome = "Ricardo";
-cliente1.cpf = 11122233309;
 
-
-const contaCorrenteLucas = new ContaCorrente();
-contaCorrenteLucas.agencia = 1001;
-
-contaCorrenteLucas.depositar(2000);
-contaCorrenteLucas.sacar(300);
-contaCorrenteLucas.cliente = cliente1;
 
 // ---- //
 
-const contaCorrenteTeste = new ContaCorrente();
-contaCorrenteTeste.agencia = 1001;
 
+const cliente1 =  new Cliente('Alice' , 88822233123);
+const contaAlice = new ContaCorrente(cliente1, 100);
 
-const cliente2 = new Cliente();
-cliente2.nome = "Alice";
-cliente2.cpf = 88822233309;
+const cliente2 = new Cliente('Lucas' , 88822233309);
+const contaLucas = new ContaCorrente(cliente2, 1001);
 
-contaCorrenteTeste.cliente = cliente2;
-contaCorrenteTeste.cliente.nome = "Alice";
-contaCorrenteTeste.cliente.cpf = 88822233309;
+contaLucas.depositar(1000);
+contaLucas.transferir(500 , contaAlice);
 
+console.log(contaAlice);
+console.log(contaLucas);
 
-let valorTransferir = 200;
-contaCorrenteLucas.transferir(valorTransferir, contaCorrenteTeste);
-console.log(contaCorrenteLucas);
-console.log(contaCorrenteTeste);
-
+console.log(ContaCorrente.numeroDeContas);
