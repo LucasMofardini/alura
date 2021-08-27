48331085ea5b13a18d3588a5ae05f0611c7ca4a3
Lucas Mofardini <lucasmofardini@gmail.com>
2021-08-26 23:17:27 -0300

    [REACT] 5

--- Formação/React/React-1/ceep/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ Formação/React/React-1/ceep/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -2,11 +2,22 @@ import React, { Component } from "react";
 import "./estiloFormulario.css"
 
 export default class FormularioCadastro extends Component {
-  render() {
+  constructor(){
+    //Instancia com nada
+    super();
+    this.titulo = "";
+  
+  }
+
+  handleMudancaTitulo(event){
+  this.titulo = event.target.value;  
+  console.log(this.titulo);
+  }
+  render() {  
     return (
       <form className="form-cadastro">
-        <input type="text" placeholder="Titulo" />
-        <textarea placeholder="Escreva Sua nota ..."></textarea>
+        <input type="text" placeholder="Titulo" onChange={this.handleMudancaTitulo.bind(this)} />
+        <textarea  placeholder="Escreva Sua nota ..."></textarea>
         <button type="submit">Criar Nota</button>
       </form>
     );
