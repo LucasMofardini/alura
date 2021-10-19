717280526a3af283ab6f7abd1a75b668fbee39f6
Lucas Mofardini <lucasmofardini@gmail.com>
2021-10-18 22:57:21 -0300

    [NODE]

--- Node/config/customExpress.js
+++ Node/config/customExpress.js
@@ -1,10 +1,14 @@
 // Express é um modulo para criar servidores, modulos
 const express = require('express');
 const consign = require('consign');
+const bodyParser = require('body-parser');
 
 module.exports = () => {
     const app = express();
-
+    app.use(bodyParser.urlencoded({
+        extended: true
+    }));
+    app.use(bodyParser.json());
     //Todo controller que for colocado na pasta controller vai ser requerido no app
     consign()
         .include('controllers')
--- Node/controllers/atendimentos.js
+++ Node/controllers/atendimentos.js
@@ -1,8 +1,16 @@
 module.exports = app => {
+    //Duas rotas : GET e SET para o '/atendimentos'
 
     // req : o que enviaram para a gente na porta 3000;
     // res : o que vamos devolver para ele
+    // pegando dados do diretorio atendimentos
     app.get('/atendimentos', (req, res) => {
-        res.send('Você esta na rota de atendimentos');
+        res.send('Você esta na rota de atendimentos e está realizando um GET');
+    });
+
+    // enviando dados no diretorio atendimentos
+    app.post('/atendimentos', (req, res) => {
+        console.log(req.body);
+        res.send('Voce está na rota de atendimentos e está realizando um POST');
     });
 }
