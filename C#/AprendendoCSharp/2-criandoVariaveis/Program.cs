2a4d604144ab055f1061f0b55c5d81fa98888af6
Lucas Mofardini <lucasmofardini@gmail.com>
2022-10-05 22:52:15 -0300

    [C#] Aula 3

---
+++ C#/AprendendoCSharp/2-criandoVariaveis/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto  2 - Criando Variáveis");
+        int idade;
+        idade = 20;
+
+        idade = idade - 5;
+
+        Console.WriteLine("A idade é de : " + idade);
+
+        Console.WriteLine("Projeto 3 - Criando Variáveis ponto flutuante");
+
+        double salario = 3000.12;
+        Console.WriteLine(salario);
+
+        Console.WriteLine("Projeto 4 - Conversoes e outros tipos");
+
+        double salario2 = 3000.10;
+        int salarioInteiro = (int)salario2;
+
+        //Numeros grandes
+        long numGrande = 1011230191203139;
+
+        //Numeros pequenos
+        short numPequeno = 10;
+
+        float altura = 1.62f;
+
+
+        Console.WriteLine("Tecle enter para sair ...");
+        Console.ReadLine();
+    }
+}
